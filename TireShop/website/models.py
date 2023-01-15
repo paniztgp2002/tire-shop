e45f67b2ee25c9ee3cbd0bb66817744c8753b6ef
paniztgp2002 <82807710+paniztgp2002@users.noreply.github.com>
2023-01-15 19:35:53 +0330

    Update models.py

--- TireShop/website/models.py
+++ TireShop/website/models.py
@@ -7,20 +7,22 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=11)
     balance = models.IntegerField(default=0)
     def last_address(self):
-        return CustomerHasCart.objects.filter(customer_id=self.id).latest('address')
+        return Cart.objects.filter(customer_id=self.id).latest('address')
     def last_postcode(self):
-        return CustomerHasCart.objects.filter(customer_id=self.id).latest('postcode')
+        return Cart.objects.filter(customer_id=self.id).latest('postcode')
 
 class Cart(models.Model):
     adress = models.CharField(max_length=200)
     Paid = models.BooleanField()
-    DateTime= models.DateField(auto_now_add=False)
+    datetime= models.DateField(auto_now_add=False)
     Postcode = models.CharField(max_length=20)
+    customer_id = models.IntegerField()
     #totalprice
-
+    
 class CustomerHasCart(models.Model):
     customer_id = models.IntegerField()
     cart_id = models.IntegerField()
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     brand = models.CharField(max_length=30)
@@ -33,11 +35,10 @@ class Tire(models.Model):
 class rime(models.Model):
     description = models.CharField(max_length=5)
     layer = models.CharField(max_length=20)
-    pattern = models.CharField(max_length=20)
-class Seller(models.Model):
+    pattern = models.CharField(max_length=20)         
+class deal(models.Model):
+    datetime = models.DateField(auto_now_add=False)
+    seald = models.BooleanField()
+class organization(models.Model):    
     name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=11)
-    balance = models.IntegerField(default=0)
-class Organization(models.Model):
-    name = models.CharField(max_length=50)
-                     
+
