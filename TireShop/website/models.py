c6170258671fc60e95f4d849a839295c21730ce2
farimahmh <82808697+farimahmh@users.noreply.github.com>
2023-01-15 19:24:17 +0330

    Update models.py

--- TireShop/website/models.py
+++ TireShop/website/models.py
@@ -8,6 +8,8 @@ class Customer(models.Model):
     balance = models.IntegerField(default=0)
     def last_address(self):
         return CustomerHasCart.objects.filter(customer_id=self.id).latest('address')
+    def last_postcode(self):
+        return CustomerHasCart.objects.filter(customer_id=self.id).latest('postcode')
 
 class Cart(models.Model):
     adress = models.CharField(max_length=200)
@@ -15,8 +17,20 @@ class Cart(models.Model):
     DateTime= models.DateField(auto_now_add=False)
     Postcode = models.CharField(max_length=20)
     #totalprice
-
+    
 class CustomerHasCart(models.Model):
     customer_id = models.IntegerField()
     cart_id = models.IntegerField()
-
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=30)
+class Tube(models.Model):
+    size = models.CharField(max_length=5)
+class Tire(models.Model):
+    description = models.CharField(max_length=200)
+    layer = models.CharField(max_length=20)
+    pattern = models.CharField(max_length=20)
+class rime(models.Model):
+    description = models.CharField(max_length=5)
+    layer = models.CharField(max_length=20)
+    pattern = models.CharField(max_length=20)         
