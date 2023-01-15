a9e4c461923eefa9bd528296fc1ebf1efb8c45ff
farimahmh <82808697+farimahmh@users.noreply.github.com>
2023-01-15 20:54:03 +0330

    fixes

--- TireShop/website/views.py
+++ TireShop/website/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'website/index.html', {})
+def login(request):
+    return render(request, 'website/login.html', {})    
 
