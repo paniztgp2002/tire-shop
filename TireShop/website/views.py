2d3d8a4976f06ca7239b38aab98fd29f035ac4e4
morteza24 <mortezia4444.mz@gmail.com>
2023-01-16 02:06:11 +0330

    fixed views.py

--- TireShop/website/views.py
+++ TireShop/website/views.py
@@ -4,16 +4,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'website/index.html', {})
+
+
 def login(request):
     return render(request, 'website/login.html', {})
+
+
 def signup(request):
     return render(request, 'website/signup.html', {})        
+
+
 def product_page(request):
-<<<<<<< Updated upstream
     return render(request, 'website/product_page.html', {})        
-=======
-    return render(request, 'website/product-page.html', {})
+
+
 def products(request):
     return render(request, 'website/products.html', {})            
->>>>>>> Stashed changes
-
