a6a42dd4743843b49182607af2b4e375755bde2b
paniztgp2002 <82807710+paniztgp2002@users.noreply.github.com>
2023-01-15 22:06:19 +0330

    update

--- TireShop/website/views.py
+++ TireShop/website/views.py
@@ -9,5 +9,5 @@ def login(request):
 def signup(request):
     return render(request, 'website/signup.html', {})        
 def product_page(request):
-    return render(request, 'website/product-page.html', {})        
+    return render(request, 'website/product_page.html', {})        
 
